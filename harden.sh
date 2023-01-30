ff4c62323eac9154dbf04ac2359164d059a5b2cb
0r4n63Ju1c3 <69871347+0r4n63Ju1c3@users.noreply.github.com>
2023-01-29 21:07:23 -0500

    Add files via upload
    Initial shell script to harden system

---
+++ harden.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+#run this in a public setting
+
+if [ "$EUID" -ne 0 ]
+  then echo "Not ROOT"
+    echo "quiting..."
+  exit
+fi
+
+echo "printing status"
+systemctl status > serviceOut.txt
+cat /etc/systemd/network/20-wlan.network | grep -i dns >> serviceOut.txt
+cat /etc/systemd/resolved.conf.d/dns_servers.conf | grep -i dns >> serviceOut.txt
+cat /etc/NetworkManager/conf.d/dns.conf >> serviceOut.txt
+
+echo "Changing DNS..."
+
+read -p "Use Cloudflare && Google? [Y/n] " ans && [[ $ans == [yY] || $ans == [yY][eE][sS] ]] || exit
+
+rm /etc/resolv.conf
+touch /etc/resolv.conf 
+cat > /etc/resolv.conf <<-EOF
+    nameserver 1.1.1.1
+    nameserver 8.8.8.8
+EOF
+chattr +i /etc/resolv.conf 
+
+cat > /etc/systemd/resolved.conf/dns_servers.conf <<-EOF 
+    [Resolve]
+    # Some examples of DNS servers which may be used for DNS= and FallbackDNS=:
+    # Cloudflare: 1.1.1.1#cloudflare-dns.com 1.0.0.1#cloudflare-dns.com 2606:4700:4700::1111#cloudflare-dns.com 2606:4700:4700::1001#cloudflare-dns.com
+    # Google:     8.8.8.8#dns.google 8.8.4.4#dns.google 2001:4860:4860::8888#dns.google 2001:4860:4860::8844#dns.google
+    # Quad9:      9.9.9.9#dns.quad9.net 149.112.112.112#dns.quad9.net 2620:fe::fe#dns.quad9.net 2620:fe::9#dns.quad9.net
+    DNS=1.1.1.1 2606:4700:4700::1111
+    FallbackDNS=8.8.8.8
+    Domains=~.
+    DNSSEC=true
+    DNSOverTLS=opportunistic
+    MulticastDNS=no
+    LLMNR=no
+    #Cache=yes
+    #CacheFromLocalhost=no
+    #DNSStubListener=yes
+    #DNSStubListenerExtra=
+    #ReadEtcHosts=yes
+    #ResolveUnicastSingleLabel=no
+EOF 
+
+cat > /etc/NetworkManager/conf.d/dns.conf <<-EOF
+    [main]
+    dns=none
+    systemd-resolved=false
+EOF
+
+systemctl daemon-reload
+systemctl restart systemd-resolved
+systemctl restart systemd-networkd.service
+ 
+echo -e "Services Shutting Down"
+#avahi and cups
+systemctl disable avahi-daemon.service
+systemctl stop avahi-daemon.service
+systemctl disable avahi-daemon.socket
+systemctl stop avahi-daemon.socket
+systemctl disable cups
+systemctl stop cups
+
+
+systemctl daemon-reload
+systemctl restart systemd-resolved
+systemctl restart systemd-networkd.service
+ 
+systemd-resolve --interface=wlan0 --set-dns 1.1.1.1
+
+resolvectl status
+
+echo -e "Adding IPTABLES rules..."
+
+
+#current interface 
+#/etc/systemd/network/20-wlan.network 
+
+#drop ins:
+#/etc/systemd/resolved.conf.d/dns_servers.conf
+#/etc/NetworkManager/conf.d/dns.conf
+
+#chattr -i /etc/resolv.conf
+#ln -s /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
